71126f4eccc748b1317d8eda6d60fc7bf8d32333
Dhee-codes <divine.e.obiorah@gmail.com>
2026-02-11 15:25:15 +0100

    Make footer responsive

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,12 +2,12 @@ import Link from "next/link";
 
 export const Footer = () => {
   return (
-    <footer className="text-brand">
-      <div className="text-white text-sz-48 tracking-wide uppercase leading-17.5">
+    <footer className="text-brand md:text-center md:grid">
+      <div className="text-white text-sz-48 tracking-wide uppercase leading-17.5 mb-4 md:mb-20.75 md:col-start-2 col-span-2">
         SpryOne
       </div>
-      <nav>
-        <ul className="text-sz-16 mb-6 md:mb-0">
+      <nav className="md:col-start-2 col-span-2">
+        <ul className="text-sz-16 mb-6 md:mb-0 space-y-2 md:space-y-0 md:flex md:justify-center md:gap-sz-60">
           <li>
             <Link href="">Home</Link>
           </li>
@@ -25,15 +25,11 @@ export const Footer = () => {
           </li>
         </ul>
       </nav>
-      <div>
-        <ul className="text-sz-20 flex gap-sz-28">
-          <li>
-            <Link href="">Terms & Conditions</Link>
-          </li>
-          <li>
-            <Link href="">Privacy Policy</Link>
-          </li>
-        </ul>
+      <div className="text-sz-16 md:text-sz-20 mb-2 md:mb-0 md:col-start-1 md:row-start-2">
+        <Link href="">Terms & Conditions</Link>
+      </div>
+      <div className="text-sz-16 md:text-sz-20 md:col-start-4">
+        <Link href="">Privacy Policy</Link>
       </div>
     </footer>
   )
